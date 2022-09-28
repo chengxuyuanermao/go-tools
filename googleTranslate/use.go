07a042dea7a2aad880d92ec402b580c12cebbfa8
chenwx <chenwx@flatincbr.com>
2022-09-28 14:12:32 +0800

    谷歌翻译工具

--- googleTranslate/use.go
+++ googleTranslate/use.go
@@ -27,8 +27,15 @@ func TranslateEn2Ch(text string) (string, error) {
 		return "", err
 	}
 
+	// 返回的bs是一个json格式的数据，用的是 byte切片存储
+	/**
+	[[["Programmer Er Mao is a good public account","程序员二毛是个不错的公众号",null,null,3,null,null,[[]],[[["2a0054a65181c79d122abe05e57adef1","zh_en_2021q4.md"]]]]],null,"zh-CN",null,null,null,null,[]]
+	*/
 	//return string(bs), nil
-	//返回的json反序列化比较麻烦, 直接字符串拆解
+
+	// 1 反序列化。由于 json 中同一维度既含有字符串，又含有数组，为复合型的，所以比较麻烦。
+
+	// 2 直接字符串拆解
 	ss := string(bs)
 	ss = strings.ReplaceAll(ss, "[", "")
 	ss = strings.ReplaceAll(ss, "]", "")
@@ -44,4 +51,19 @@ func use() {
 		fmt.Println(err)
 	}
 	fmt.Println(str)
+}
+
+type translateResp struct {
+	param0 [][]interface{}
+	param1 interface{}
+	param2 interface{}
+	param3 interface{}
+	param4 interface{}
+	param5 interface{}
+	param6 interface{}
+	param7 interface{}
+}
+
+type translateResps struct {
+	translateResp []translateResp
 }
