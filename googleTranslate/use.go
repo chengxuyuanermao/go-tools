d1344196335fc606fabebab522bf9ef07defc50f
chenwx <chenwx@flatincbr.com>
2022-09-28 16:04:41 +0800

    谷歌翻译工具-json反序列化获取，三维断言

--- googleTranslate/use.go
+++ googleTranslate/use.go
@@ -1,11 +1,11 @@
 package googleTranslate
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strings"
 )
 
 func TranslateEn2Ch(text string) (string, error) {
@@ -29,41 +29,34 @@ func TranslateEn2Ch(text string) (string, error) {
 
 	// 返回的bs是一个json格式的数据，用的是 byte切片存储
 	/**
-	[[["Programmer Er Mao is a good public account","程序员二毛是个不错的公众号",null,null,3,null,null,[[]],[[["2a0054a65181c79d122abe05e57adef1","zh_en_2021q4.md"]]]]],null,"zh-CN",null,null,null,null,[]]
+	[[["this is testing","这是一个测试",null,null,3,null,null,[[]],[[["2a0054a65181c79d122abe05e57adef1","zh_en_2021q4.md"]]]]],null,"zh-CN",null,null,null,null,[]]
 	*/
 	//return string(bs), nil
 
 	// 1 反序列化。由于 json 中同一维度既含有字符串，又含有数组，为复合型的，所以比较麻烦。
+	var str []interface{}
+	err = json.Unmarshal(bs, &str)
+	if err != nil {
+		fmt.Println("err: ", err)
+	}
+	arr1 := str[0].([]interface{}) // 三维数组
+	arr2 := arr1[0].([]interface{})
+	return arr2[0].(string), nil
 
 	// 2 直接字符串拆解
-	ss := string(bs)
-	ss = strings.ReplaceAll(ss, "[", "")
-	ss = strings.ReplaceAll(ss, "]", "")
-	ss = strings.ReplaceAll(ss, "null,", "")
-	ss = strings.Trim(ss, `"`)
-	ps := strings.Split(ss, `","`)
-	return ps[0], nil
+	//ss := string(bs)
+	//ss = strings.ReplaceAll(ss, "[", "")
+	//ss = strings.ReplaceAll(ss, "]", "")
+	//ss = strings.ReplaceAll(ss, "null,", "")
+	//ss = strings.Trim(ss, `"`)
+	//ps := strings.Split(ss, `","`)
+	//return ps[0], nil
 }
 
 func use() {
-	str, err := TranslateEn2Ch("程序员二毛是个不错的公众号")
+	str, err := TranslateEn2Ch("这是一个测试")
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(str)
-}
-
-type translateResp struct {
-	param0 [][]interface{}
-	param1 interface{}
-	param2 interface{}
-	param3 interface{}
-	param4 interface{}
-	param5 interface{}
-	param6 interface{}
-	param7 interface{}
-}
-
-type translateResps struct {
-	translateResp []translateResp
 }
--- main.go
+++ main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+type cjsonElem struct {
+	GuildName string `json:"guildName"`
+	NickName  string `json:"nickName"`
+	UserId    int32  `json:"userId"`
+	UserName  string `json:"userName"`
 }
+
+type cjson struct {
+	Code    int32       `json:"code"`
+	Data    []cjsonElem `json:"data"`
+	Message string      `json:"message"`
+}
+
+func main() {
+	var str string
+	str =
+		`{
+		"code": 10,
+		"data": [
+			{
+				"guildName": "Test1",
+				"nickName": "def",
+				"userId": 10025,
+				"userName": "abc"
+			},
+			{
+				"guildName": "Test2",
+				"nickName": "def",
+				"userId": 10026,
+				"userName": "yxz"
+			}
+		],
+		"message": "success"
+	}`
+	val := cjson{}
+	err := json.Unmarshal([]byte(str), &val)
+	if err != nil {
+		fmt.Println("Unmarshal failed, err:", err)
+		return
+	}
+	fmt.Println("val:", val)
+	fmt.Println(val.Data[0].GuildName)
+}
+
+/**
+输出结果：
+val: {10 [{Test1 def 10025 abc} {Test2 def 10026 yxz}] success}
+Test1
+*/
